81fe2dd2d764bbf941258c12bae9c955088ef140
Kristian A. Hiorth <kristahi@ifi.uio.no>
2018-10-07 16:30:48 +0200

    Blob-commit of various Radiotap fixes, need to clean up if submitting patches...

--- dpkt/radiotap.py
+++ dpkt/radiotap.py
@@ -10,73 +10,58 @@ from . import ieee80211
 from .decorators import deprecated
 
 # Ref: http://www.radiotap.org
-# Fields Ref: http://www.radiotap.org/defined-fields/all
 
 # Present flags
-_TSFT_MASK = 0x1000000
-_FLAGS_MASK = 0x2000000
-_RATE_MASK = 0x4000000
-_CHANNEL_MASK = 0x8000000
-_FHSS_MASK = 0x10000000
-_ANT_SIG_MASK = 0x20000000
-_ANT_NOISE_MASK = 0x40000000
-_LOCK_QUAL_MASK = 0x80000000
-_TX_ATTN_MASK = 0x10000
-_DB_TX_ATTN_MASK = 0x20000
-_DBM_TX_POWER_MASK = 0x40000
-_ANTENNA_MASK = 0x80000
-_DB_ANT_SIG_MASK = 0x100000
-_DB_ANT_NOISE_MASK = 0x200000
-_RX_FLAGS_MASK = 0x400000
-_CHANNELPLUS_MASK = 0x200
-_EXT_MASK = 0x1
+_TSFT = 0
+_FLAGS = 1
+_RATE = 2
+_CHANNEL = 3
+_FHSS = 4
+_ANT_SIG = 5
+_ANT_NOISE = 6
+_LOCK_QUAL = 7
+_TX_ATTN = 8
+_DB_TX_ATTN = 9
+_DBM_TX_POWER = 10
+_ANTENNA = 11
+_DB_ANT_SIG = 12
+_DB_ANT_NOISE = 13
+_RX_FLAGS = 14
+_CHANNELPLUS = 18 # Not quite standardized
+_EXT = 31
 
-_TSFT_SHIFT = 24
-_FLAGS_SHIFT = 25
-_RATE_SHIFT = 26
-_CHANNEL_SHIFT = 27
-_FHSS_SHIFT = 28
-_ANT_SIG_SHIFT = 29
-_ANT_NOISE_SHIFT = 30
-_LOCK_QUAL_SHIFT = 31
-_TX_ATTN_SHIFT = 16
-_DB_TX_ATTN_SHIFT = 17
-_DBM_TX_POWER_SHIFT = 18
-_ANTENNA_SHIFT = 19
-_DB_ANT_SIG_SHIFT = 20
-_DB_ANT_NOISE_SHIFT = 21
-_RX_FLAGS_SHIFT = 22
-_CHANNELPLUS_SHIFT = 10
-_EXT_SHIFT = 0
-
+""" # FIXME unused?
 # Flags elements
 _FLAGS_SIZE = 2
-_CFP_FLAG_SHIFT = 0
-_PREAMBLE_SHIFT = 1
-_WEP_SHIFT = 2
-_FRAG_SHIFT = 3
-_FCS_SHIFT = 4
-_DATA_PAD_SHIFT = 5
-_BAD_FCS_SHIFT = 6
-_SHORT_GI_SHIFT = 7
+_CFP_FLAG = 0
+_PREAMBLE = 1
+_WEP = 2
+_FRAG = 3
+_FCS = 4
+_DATA_PAD = 5
+_BAD_FCS = 6
+_SHORT_GI = 7"""
 
 # Channel type
 _CHAN_TYPE_SIZE = 4
-_CHANNEL_TYPE_SHIFT = 4
-_CCK_SHIFT = 5
-_OFDM_SHIFT = 6
-_TWO_GHZ_SHIFT = 7
-_FIVE_GHZ_SHIFT = 8
-_PASSIVE_SHIFT = 9
-_DYN_CCK_OFDM_SHIFT = 10
-_GFSK_SHIFT = 11
-_GSM_SHIFT = 12
-_STATIC_TURBO_SHIFT = 13
-_HALF_RATE_SHIFT = 14
-_QUARTER_RATE_SHIFT = 15
+_CHANNEL_TYPE = 4
+_TURBO = 4
+_CCK = 5
+_OFDM = 6
+_TWO_GHZ = 7
+_FIVE_GHZ = 8
+_PASSIVE = 9
+_DYN_CCK_OFDM = 10
+""" # FIXME unused
+_GFSK = 11
+_GSM = 12
+_STATIC_TURBO = 13
+_HALF_RATE = 14
+_QUARTER_RATE = 15
+"""
 
 # Flags offsets and masks
-_FCS_SHIFT = 4
+_FCS = 4
 _FCS_MASK = 0x10
 
 
@@ -97,147 +82,160 @@ class Radiotap(dpkt.Packet):
         ('present_flags', 'I', 0)
     )
 
+    __byte_order__ = '<'
+
+    def _pf_getter(self, flag):
+        return (self.present_flags & (2**flag)) >> flag
+
+    def _pf_setter(self, val, flag):
+        if val:
+            self.present_flags |= 2**flag
+        else:
+            self.present_flags &= ~(2**flag)
+
     @property
     def tsft_present(self):
-        return (self.present_flags & _TSFT_MASK) >> _TSFT_SHIFT
+        return self. _pf_getter(_TSFT)
 
     @tsft_present.setter
     def tsft_present(self, val):
-        self.present_flags |= val << _TSFT_SHIFT
+        self._pf_setter(val, _TSFT)
 
     @property
     def flags_present(self):
-        return (self.present_flags & _FLAGS_MASK) >> _FLAGS_SHIFT
+        return self._pf_getter(_FLAGS)
 
     @flags_present.setter
     def flags_present(self, val):
-        self.present_flags |= val << _FLAGS_SHIFT
+        self._pf_setter(val, _FLAGS)
 
     @property
     def rate_present(self):
-        return (self.present_flags & _RATE_MASK) >> _RATE_SHIFT
+        return self._pf_getter(_RATE)
 
     @rate_present.setter
     def rate_present(self, val):
-        self.present_flags |= val << _RATE_SHIFT
+        self._pf_setter(val, _RATE)
 
     @property
     def channel_present(self):
-        return (self.present_flags & _CHANNEL_MASK) >> _CHANNEL_SHIFT
+        return self._pf_getter(_CHANNEL)
 
     @channel_present.setter
     def channel_present(self, val):
-        self.present_flags |= val << _CHANNEL_SHIFT
+        self._pf_setter(val, _CHANNEL)
 
     @property
     def fhss_present(self):
-        return (self.present_flags & _FHSS_MASK) >> _FHSS_SHIFT
+        return self._pf_getter(_FHSS)
 
     @fhss_present.setter
     def fhss_present(self, val):
-        self.present_flags |= val << _FHSS_SHIFT
+        self._pf_setter(val, _FHSS)
 
     @property
     def ant_sig_present(self):
-        return (self.present_flags & _ANT_SIG_MASK) >> _ANT_SIG_SHIFT
+        return self._pf_getter(_ANT_SIG)
 
     @ant_sig_present.setter
     def ant_sig_present(self, val):
-        self.present_flags |= val << _ANT_SIG_SHIFT
+        self._pf_setter(val, _ANT_SIG)
 
     @property
     def ant_noise_present(self):
-        return (self.present_flags & _ANT_NOISE_MASK) >> _ANT_NOISE_SHIFT
+        return self._pf_getter(_ANT_NOISE)
 
     @ant_noise_present.setter
     def ant_noise_present(self, val):
-        self.present_flags |= val << _ANT_NOISE_SHIFT
+        self._pf_setter(val, _ANT_NOISE)
 
     @property
     def lock_qual_present(self):
-        return (self.present_flags & _LOCK_QUAL_MASK) >> _LOCK_QUAL_SHIFT
+        return self._pf_getter(_LOCK_QUAL)
 
     @lock_qual_present.setter
     def lock_qual_present(self, val):
-        self.present_flags |= val << _LOCK_QUAL_SHIFT
+        self._pf_setter(val, _LOCK_QUAL)
 
     @property
     def tx_attn_present(self):
-        return (self.present_flags & _TX_ATTN_MASK) >> _TX_ATTN_SHIFT
+        return self._pf_getter(_TX_ATTN)
 
     @tx_attn_present.setter
     def tx_attn_present(self, val):
-        self.present_flags |= val << _TX_ATTN_SHIFT
+        self._pf_setter(val, _TX_ATTN)
 
     @property
     def db_tx_attn_present(self):
-        return (self.present_flags & _DB_TX_ATTN_MASK) >> _DB_TX_ATTN_SHIFT
+        return self._pf_getter(_DB_TX_ATTN)
 
     @db_tx_attn_present.setter
     def db_tx_attn_present(self, val):
-        self.present_flags |= val << _DB_TX_ATTN_SHIFT
+        self._pf_setter(val, _DB_TX_ATTN)
 
     @property
     def dbm_tx_power_present(self):
-        return (self.present_flags & _DBM_TX_POWER_MASK) >> _DBM_TX_POWER_SHIFT
+        return self._pf_getter(_DBM_TX_POWER)
 
     @dbm_tx_power_present.setter
     def dbm_tx_power_present(self, val):
-        self.present_flags |= val << _DBM_TX_POWER_SHIFT
+        self._pf_setter(val, _DBM_TX_POWER)
 
     @property
     def ant_present(self):
-        return (self.present_flags & _ANTENNA_MASK) >> _ANTENNA_SHIFT
+        return self._pf_getter(_ANTENNA)
 
     @ant_present.setter
     def ant_present(self, val):
-        self.present_flags |= val << _ANTENNA_SHIFT
+        self._pf_setter(val, _ANTENNA)
 
     @property
     def db_ant_sig_present(self):
-        return (self.present_flags & _DB_ANT_SIG_MASK) >> _DB_ANT_SIG_SHIFT
+        return self._pf_getter(_DB_ANT_SIG)
 
     @db_ant_sig_present.setter
     def db_ant_sig_present(self, val):
-        self.present_flags |= val << _DB_ANT_SIG_SHIFT
+        self._pf_setter(val, _DB_ANT_SIG)
 
     @property
     def db_ant_noise_present(self):
-        return (self.present_flags & _DB_ANT_NOISE_MASK) >> _DB_ANT_NOISE_SHIFT
+        return self._pf_getter(_DB_ANT_NOISE)
 
     @db_ant_noise_present.setter
     def db_ant_noise_present(self, val):
-        self.present_flags |= val << _DB_ANT_NOISE_SHIFT
+        self._pf_setter(val, _DB_ANT_NOISE)
 
     @property
     def rx_flags_present(self):
-        return (self.present_flags & _RX_FLAGS_MASK) >> _RX_FLAGS_SHIFT
+        return self._pf_getter(_RX_FLAGS)
 
     @rx_flags_present.setter
     def rx_flags_present(self, val):
-        self.present_flags |= val << _RX_FLAGS_SHIFT
+        self._pf_setter(val, _RX_FLAGS)
 
     @property
     def chanplus_present(self):
-        return (self.present_flags & _CHANNELPLUS_MASK) >> _CHANNELPLUS_SHIFT
+        return self._pf_getter(_CHANNELPLUS)
 
     @chanplus_present.setter
     def chanplus_present(self, val):
-        self.present_flags |= val << _CHANNELPLUS_SHIFT
+        self._pf_setter(val, _CHANNELPLUS)
 
     @property
     def ext_present(self):
-        return (self.present_flags & _EXT_MASK) >> _EXT_SHIFT
+        return self._pf_getter(_EXT)
 
     @ext_present.setter
     def ext_present(self, val):
-        self.present_flags |= val << _EXT_SHIFT
+        self._pf_setter(val, _EXT)
 
     def unpack(self, buf):
         dpkt.Packet.unpack(self, buf)
-        self.data = buf[socket.ntohs(self.length):]
+        #self.data = buf[socket.ntohs(self.length):]
+        self.data = buf[self.length:]
 
         self.fields = []
+        self.raw_radiotap = buf[:self.length]
         buf = buf[self.__hdr_len__:]
 
         # decode each field into self.<name> (eg. self.tsft) as well as append it self.fields list
@@ -293,6 +291,67 @@ class Radiotap(dpkt.Packet):
             ('flags', 'H', 0),
         )
 
+        __byte_order__ = '<'
+
+        @property
+        def turbo(self): return (self.flags & (2**_TURBO)) >> _TURBO
+
+        @turbo.setter
+        def turbo(self):
+            self.flags |= val << _TURBO
+
+        @property
+        def cck(self): return (self.flags & (2**_CCK)) >> _CCK
+
+        @cck.setter
+        def cck(self):
+            self.flags |= val << _CCK
+
+        @property
+        def ofdm(self): return (self.flags & (2**_OFDM)) >> _OFDM
+
+        @ofdm.setter
+        def ofdm(self):
+            self.flags |= val << _OFDM
+
+        @property
+        def two_ghz(self): return (self.flags & (2**_TWO_GHZ)) >> _TWO_GHZ
+
+        @two_ghz.setter
+        def two_ghz(self):
+            self.flags |= val << _TWO_GHZ
+
+        @property
+        def five_ghz(self): return (self.flags & (2**_FIVE_GHZ)) >> _FIVE_GHZ
+
+        @five_ghz.setter
+        def five_ghz(self):
+            self.flags |= val << _FIVE_GHZ
+
+        @property
+        def passive(self): return (self.flags & (2**_PASSIVE)) >> _PASSIVE
+
+        @passive.setter
+        def passive(self):
+            self.flags |= val << _PASSIVE
+
+        @property
+        def dyn_cck_ofdm(self): return (self.flags & (2**_DYN_CCK_OFDM)) >> _DYN_CCK_OFDM
+
+        @dyn_cck_ofdm.setter
+        def dyn_cck_ofdm(self):
+            self.flags |= val << _DYN_CCK_OFDM
+
+        @property
+        def gfsk(self): return (self.flags & (2**_GFSK)) >> _GFSK
+
+        @gfsk.setter
+        def gfsk(self):
+            self.flags |= val << _GFSK
+
+        # Wireshark also supports several other flags defined in the non-standard
+        # Channel+/ChannelX field type, not including them for now.
+
     class FHSS(dpkt.Packet):
         __hdr__ = (
             ('set', 'B', 0),
@@ -305,11 +364,14 @@ class Radiotap(dpkt.Packet):
         )
 
         @property
-        def fcs(self): return (self.val & _FCS_MASK) >> _FCS_SHIFT
+        def fcs(self): return (self.val & _FCS_MASK) >> _FCS
 
-        # TODO statement seems to have no effect
         @fcs.setter
-        def fcs(self, v): (v << _FCS_SHIFT) | (self.val & ~_FCS_MASK)
+        def fcs(self, v):
+            if v:
+                self.val |= _FCS_MASK
+            else:
+                self.val &= ~_FCS_MASK
 
 
     class LockQuality(dpkt.Packet):
@@ -322,6 +384,8 @@ class Radiotap(dpkt.Packet):
             ('val', 'H', 0),
         )
 
+        __byte_order__ = '<'
+
     class Rate(dpkt.Packet):
         __hdr__ = (
             ('val', 'B', 0),
@@ -332,15 +396,21 @@ class Radiotap(dpkt.Packet):
             ('usecs', 'Q', 0),
         )
 
+        __byte_order__ = '<'
+
     class TxAttenuation(dpkt.Packet):
         __hdr__ = (
             ('val', 'H', 0),
         )
 
+        __byte_order__ = '<'
+
     class DbTxAttenuation(dpkt.Packet):
         __hdr__ = (
             ('db', 'H', 0),
         )
+
+        __byte_order__ = '<'
 
     class DbAntennaNoise(dpkt.Packet):
         __hdr__ = (
@@ -359,7 +429,7 @@ class Radiotap(dpkt.Packet):
 
 
 def test_Radiotap():
-    s = b'\x00\x00\x00\x18\x6e\x48\x00\x00\x00\x02\x6c\x09\xa0\x00\xa8\x81\x02\x00\x00\x00\x00\x00\x00\x00'
+    s = b'\x00\x00\x18\x00\x6e\x48\x00\x00\x00\x02\x6c\x09\xa0\x00\xa8\x81\x02\x00\x00\x00\x00\x00\x00\x00'
     rad = Radiotap(s)
     assert(rad.version == 0)
     assert(rad.present_flags == 0x6e480000)
